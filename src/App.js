a847aadd53fe68340f4d8fd4adeffe6362c5987a
abhishek ranjan <imabhishekranjann1001@gmail.com>
2023-01-22 11:35:45 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar';
 import ALLState from './context/authState';
 import Home from './components/Home';
+import About from './components/About'
 import GraphArea from './components/GraphArea';
 import Dashboard from './components/Dashboard';
+import GetNews from './components/GetNews';
+import Contact from './components/Contact';
+import { Chart } from 'chart.js';
+import Login from './components/login';
+import SignUp from './components/SignUp';
 function App() {
 
 
@@ -24,9 +30,14 @@ function App() {
           <div className="display-content">
 
             <Routes>
-              <Route exact path='/' element={<Home />} />
-              <Route exact path='/dashboard' element={<Dashboard />} />
+              {/* <Route exact path='/' element={<Home />} /> */}
+              <Route exact path='/news' element={<GetNews />} />
+     
+              <Route exact path='/' element={<Dashboard />} />
+              <Route exact path='/about' element={<About />} />
+              <Route exact path='/contact' element={<Contact />} />
               <Route exact path='/fullchart' element={<GraphArea />} />
+
 
 
             </Routes>
